d1097c2e49fcd573c23c162fb4d8a3af9e38aaa4
npcdazai <prathammandavkar932@gmail.com>
2024-09-19 13:53:36 +0530

    New add on

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -30,7 +30,7 @@ const Connect = () => {
         </Text>
         <WdiButton Context="Let’s Talk" />
       </Box>
-      <Image w="50%" zIndex={0} src={circles} />
+      <Image  h="600px" zIndex={0} src={circles} />
     </Container>
   );
 };
